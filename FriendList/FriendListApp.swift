8cc223de847fe1686e6a0f76ab9d484ea6c4dbc9
chef-nic <nicholasajohnson@me.com>
2024-07-22 16:02:02 -0700

    Initial Commit

---
+++ FriendList/FriendListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListApp.swift
+//  FriendList
+//
+//  Created by Nicholas Johnson on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
